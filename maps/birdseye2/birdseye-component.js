0baf40e147caddb4f9db58d60898271a1f307d22
drm227 <mccarthy@acm.org>
2023-11-02 09:57:56 -0600

    Refactored afterRenderMap handler to call changeBirdseye

--- maps/birdseye2/birdseye-component.js
+++ maps/birdseye2/birdseye-component.js
@@ -19,21 +19,8 @@ define([
     }
 
     BirdseyeComponentVM.prototype.afterRenderMap = function (elements,viewModel) {
-        var leaflet = viewModel.birdseye().leaflet;
-
-        viewModel.imageMap = L.map('birdseye',leaflet.imageMap.options);
-        L.imageOverlay(leaflet.imageLayer.url,leaflet.imageLayer.bounds,leaflet.imageLayer.options).addTo(viewModel.imageMap);
-
-        viewModel.featureMap = {};
-        viewModel.features().forEach(function(feature){
-            var marker = L.marker(feature.imageMapLatLng,{title:feature.name});
-            marker.addTo(viewModel.imageMap);
-            viewModel.featureMap[feature.name] = feature;
-            marker.on('click',function(e){
-                viewModel.feature(viewModel.featureMap[e.target.options.title]);
-                $('#featureDialog').modal('show');
-            });
-        })
+        viewModel.imageMap = L.map('birdseye',viewModel.birdseye().leaflet.imageMap.options);
+        viewModel.changeBirdseye(viewModel.birdseye());
     };
 
     BirdseyeComponentVM.prototype.changeBirdseye = function (birdseye) {
@@ -48,6 +35,7 @@ define([
         L.imageOverlay(birdseye.leaflet.imageLayer.url,birdseye.leaflet.imageLayer.bounds,birdseye.leaflet.imageLayer.options).addTo(map);
 
         // Add markers for the features of the selected map.
+        this.featureMap = {};
         features.forEach(function(feature){
             var marker = L.marker(feature.imageMapLatLng,{title:feature.name});
             marker.addTo(_this.imageMap);
@@ -57,7 +45,7 @@ define([
                 $('#featureDialog').modal('show');
             });
         });
-    }
+    };
 
     return {
         template: { require: 'text!birdseye-component.html' },
