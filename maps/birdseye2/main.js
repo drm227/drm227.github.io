516b0d0a26ac5e108c88dc9938f7aa73b38ea165
drm227 <mccarthy@acm.org>
2023-12-05 09:30:09 -0700

    unchanged files in birdseye2

---
+++ maps/birdseye2/main.js
@@ -0,0 +1,29 @@
+require.config({
+    paths: {
+        knockout:   '//cdnjs.cloudflare.com/ajax/libs/knockout/3.5.0/knockout-min',
+        text:       '//cdnjs.cloudflare.com/ajax/libs/require-text/2.0.12/text',
+        leaflet:    '//cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/leaflet'
+    }
+});
+
+require(['knockout'], function(ko) {
+
+    // register the Knockout component
+    ko.components.register('birdseye-component',{require:'birdseye-component'});
+
+    // read the configuration JSON file
+    $.ajax({
+        url: 'Denver.json',
+        method: 'GET',
+        dataType: 'json'
+    })
+        .done(function(config){
+            ko.applyBindings({config:config});
+        })
+        .fail(function(jqXHR, textStatus, errorThrown){
+            alert(jqXHR.status + ' - ' + errorThrown);
+        });
+
+    // For debugging, put a reference to the Knockout module in the global namespace.
+    window.ko = ko;
+});
